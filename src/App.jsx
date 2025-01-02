845e4eb019ec218432d6001bf6b361353de2e04c
MateusPereirah <mateus.pereira1@outlook.com.br>
2025-01-02 18:17:35 -0300

    fix deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Sidebar from "./components/sidebar/sidebar";
+import Sidebar from "./components/sidebar/Sidebar";
 import Header from "./components/header/Header";
 import CartSideBar from "./components/cart/CartSideBar";
 import Main from "./components/main/main";
